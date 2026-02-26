a54d0b994fa55f916acd010be46aca8dbb578e2b
Harsh Panchal <hrppanchal27@gmail.com>
2026-02-27 01:13:39 +0530

    update packages and next config for build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: 'export',
+  distDir: 'out',
 };
 
 export default nextConfig;
